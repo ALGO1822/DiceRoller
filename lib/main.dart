2c22a60712a5e8a1d233facdedd29940d1b0be9b
ALGO1822 <favourabayomi1822@gmail.com>
2024-12-30 13:51:27 +0100

    First commit for my Dice Rolling App

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,68 @@
 // ignore_for_file: unnecessary_const
 
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: const MyProfilePage(),
+    home: const DiceRoller(),
   ));
 }
 
-class MyProfilePage extends StatelessWidget {
-  const MyProfilePage({super.key});
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
 
+  @override
+  State<DiceRoller> createState() => _DiceRollerState();
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  final List<String> diceFaces = [
+    'assets/dice1.png',
+    'assets/dice2.png',
+    'assets/dice3.png',
+    'assets/dice4.png',
+    'assets/dice5.png',
+    'assets/dice6.png',
+  ];
+
+  String currentDiceFace = 'assets/dice1.png';
+  int currentDiceNumber = 1;
+
+  final Random random = Random();
+  void rollDice() {
+    setState(() {
+      int randomFaceIndex = random.nextInt(6);
+      currentDiceFace = diceFaces[randomFaceIndex];
+      currentDiceNumber = randomFaceIndex + 1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal,
+      backgroundColor: Colors.blue,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CircleAvatar(
-              radius: 60,
-              backgroundImage: const AssetImage('assets/leaf.jpg'),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Abayomi Favour',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                color: Colors.white,
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
+        child: GestureDetector(
+          onTap: rollDice,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(currentDiceFace, width: 150, height: 150),
+              const SizedBox(height: 20),
+              Text(
+                'You rolled $currentDiceNumber',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'FLUTTER ENTHUSIASTIC',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.white.withOpacity(0.4),
-                letterSpacing: 2,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Divider(
-              color: Colors.white.withOpacity(1),
-              indent: 100,
-              endIndent: 100,
-              thickness: 0.5,
-            ),
-            const SizedBox(height: 15),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-              margin: const EdgeInsets.symmetric(horizontal: 40),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: const Row(
-                children: [
-                  Icon(Icons.phone, color: Colors.teal),
-                  SizedBox(width: 30),
-                  Text(
-                    '+2348068491489',
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Colors.teal,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: const Row(
-                children: [
-                  Icon(Icons.mail, color: Colors.teal),
-                  SizedBox(width: 30),
-                  Text(
-                    'favour123@gmail.com',
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Colors.teal,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
